3011a65681e9558b7d9d82271d96766b368210e9
Md Asif Khan <ak2130251@gmail.com>
2025-07-15 20:45:31 +0530

    Delete train.py

--- train.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import os
-
-model = tf.keras.models.load_model("trained_plant_disease_model.keras")
-
-# Save with full path and confirmation
-save_path = r"C:\Users\Md Asif Khan\OneDrive\Desktop\ML Project\New Plant Diseases Dataset(Augmented)\trained_plant_disease_model.h5"
-model.save(save_path)
-print(f"✅ Model saved at: {save_path}")
