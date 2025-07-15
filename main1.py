783f2938995658924a629c9707566ace54067226
Md Asif Khan <ak2130251@gmail.com>
2025-07-15 20:28:32 +0530

    Added model file (.h5) and test folder

--- main1.py
+++ main1.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Model Prediction Function
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_plant_disease_model.keras")
+    model = tf.keras.models.load_model("trained_plant_disease_model.h5")
     image = tf.keras.preprocessing.image.load_img(test_image, target_size=(128, 128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])  # Convert single image to batch
